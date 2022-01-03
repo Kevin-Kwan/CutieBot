e2d7ed60d70942a3177413527496081c70d0a9ea
koolk <koolkev246@gmail.com>
2022-01-03 18:12:54 -0500

    string stuff

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ const { Client, Intents } = require('discord.js');
 const Discord = require('discord.js');
 const fs = require('fs');
 // Importing this allows you to access the environment variables of the running node process
-require("dotenv").config();
+require('dotenv').config();
 const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILDS);
 const client = new Client({ intents: myIntents });
 
-// "process.env" accesses the environment variables for the running node process. PREFIX is the environment variable you defined in your .env file
+// 'process.env' accesses the environment variables for the running node process. PREFIX is the environment variable you defined in your .env file
 const prefix = process.env.PREFIX;
 
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity('yo', {type: 'WATCHING'});
 });
 
 client.commands = new Discord.Collection();
 
-const commandFolders = fs.readdirSync("./src/commands");
+const commandFolders = fs.readdirSync('./src/commands');
 
 for (const folder of commandFolders) {
-	const commandFiles = fs.readdirSync("./src/commands/${folder}").filter(file => file.endsWith(".js"));
+	const commandFiles = fs.readdirSync('./src/commands/${folder}').filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
-		const command = require("./src/commands/${folder}/${file}");
+		const command = require('./src/commands/${folder}/${file}');
 		client.commands.set(command.name, command);
 	}
 }
 console.log('Commands loaded');
 
 // This is where command reading is supposed to occur.
-client.on("messageCreate", (message) =>{
+client.on('messageCreate', (message) =>{
 /* botched, fix this later
     if (!message.content.startsWith(prefix)) return;
     else {
@@ -43,8 +43,8 @@ client.on("messageCreate", (message) =>{
     }
 });
 */
-	if (message.content == "ping") {
-		message.reply("pong");
+	if (message.content == 'ping') {
+		message.reply('pong');
 	}
 })
 
@@ -65,5 +65,5 @@ client.on('interactionCreate', async interaction => {
 });
 */
 
-// Here you can login the bot. It automatically attempts to login the bot with the environment variable you set for your bot token (either "CLIENT_TOKEN" or "DISCORD_TOKEN")
+// Here you can login the bot. It automatically attempts to login the bot with the environment variable you set for your bot token (either 'CLIENT_TOKEN' or 'DISCORD_TOKEN')
 client.login();
