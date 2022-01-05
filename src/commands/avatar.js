057deafd6d2d5b295afceee76b2daade49628738
koolk <koolkev246@gmail.com>
2022-01-05 13:53:44 -0500

    install more dependencies

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -1,3 +1,9 @@
+module.exports.execute = (client, message, args) => {
+    if (message.author.bot) return;
+    const target = message.mentions.members.first() || message.guild.users.cache.get(args[0]);
+};
+
+
 module.exports.info = {
     name: "avatar",
     alias: [""],
