1614590e2aa74afa8c8ede5558b41ec192e8b2e3
Kevin <koolkev246@gmail.com>
2023-03-30 14:53:44 -0400

    updated invite

--- src/commands/botinvite.js
+++ src/commands/botinvite.js
@@ -7,7 +7,7 @@ const { Invite } = require("discord.js");
 module.exports = {
     run: async (client, message, args) => {
     const prefix = process.env.PREFIX;
-    message.reply('https://discord.com/oauth2/authorize?client_id=927315876036898866&permissions=8&scope=bot');
+    message.reply('https://discord.com/api/oauth2/authorize?client_id=927315876036898866&scope=bot%20applications.commands');
     },
 };
 
