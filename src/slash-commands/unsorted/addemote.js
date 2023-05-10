f235bf3174ea45b619bb922bfb831a6a2782923a
Kevin <koolkev246@gmail.com>
2023-05-10 14:30:53 -0400

    added better error logging on addemote commands

--- src/slash-commands/unsorted/addemote.js
+++ src/slash-commands/unsorted/addemote.js
@@ -30,7 +30,17 @@ module.exports = {
             // create emoji using interaction
             inter.guild.emojis.create({attachment: emoteURL, name: emoteName})
                 .then(emoji => inter.reply(`✅ Succesfully Created New Emote: ${emoji.toString()}`))
-                .catch(err => inter.reply("Error creating emoji. Check the file's size (no larger than 256.0 KB) or check your command's syntax."));
+                .catch(err => {
+                    if (err.code == 50013) {
+                        message.reply("Error creating emoji. I need the `MANAGE_EMOJIS` permission to do this.");
+                    } else if (err.code == 30008) {
+                        // catch max emote limit error
+                        message.reply("Error creating emoji. This server has reached the maximum number of emotes.");
+                    } else {
+                        // catch any other error
+                        message.reply("Error creating emoji. Check the file's size (no larger than 256.0 KB) or check your command's syntax.");
+                    }
+                });
         }
     },
 };
