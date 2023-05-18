87b1f7cd35b5a6db2fa1fd8dbe2030507db3703c
Kevin <koolkev246@gmail.com>
2023-05-17 22:54:45 -0400

    botinvite slash command implemented

--- src/slash-commands/unsorted/botinvite.js
+++ src/slash-commands/unsorted/botinvite.js
@@ -8,20 +8,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('botinvite')
         .setDescription('Get the bot\'s invite link'),
-    async execute(interaction) {
-        await interaction.reply('Pong!');
-    },
-    run: async (client, message, args) => {
-    const prefix = process.env.PREFIX;
-    message.reply('Use this link to invite the bot to your server: ' + process.env.BOT_INVITE_LINK);
-    },
-};
-
-module.exports.info = {
-    name: "botinvite",
-    alias: ["invite"],
-    permission: "default",
-    category: "general",
-    guildOnly: false,
-	help: "command to get the bot's invite"
+    async execute({ client, inter }) {
+        inter.reply('Use this link to invite the bot to your server: ' + process.env.BOT_INVITE_LINK);
+    }
 };
