e33853d3fa4648df95f7a713f4b9c21489a4e2ec
Kevin <koolkev246@gmail.com>
2023-05-17 22:14:49 -0400

    say and unban slash command tested and fixed

--- src/slash-commands/unsorted/say.js
+++ src/slash-commands/unsorted/say.js
@@ -11,8 +11,9 @@ module.exports = {
                 .setRequired(true)),
     async execute({ client, inter }) {
         const saidMessage = inter.options.getString('message');
+        // the next two lines make it seem like the bot just talked with no trace of you ever using the command
         inter.deferReply();
         inter.deleteReply();
-        inter.channel.send();
+        inter.channel.send(saidMessage);
     },
 };
--- src/slash-commands/unsorted/unban.js
+++ src/slash-commands/unsorted/unban.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
