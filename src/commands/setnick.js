c1155553b3495fbc0d6f03e332fc10a54dcd3d0b
Kevin <koolkev246@gmail.com>
2023-06-24 11:51:28 -0400

    setnick command implemented

--- src/commands/setnick.js
+++ src/commands/setnick.js
@@ -1,8 +1,17 @@
+module.exports = {
+  run: async (client, message, args) => {
+    const nick = args.join(' ');
+    if (!nick) return message.channel.send('Please provide a nickname');
+    message.guild.me.setNickname(nick);
+    message.reply(`My nickname has been set to **${nick}**`);
+  },
+};
+
 module.exports.info = {
-    name: "setnick",
-    alias: [""],
-    permission: "default",
-    category: "general",
-    guildOnly: false,
-	help: ""
-}
+  name: 'setnick',
+  alias: [''],
+  permission: 'admin',
+  category: 'general',
+  guildOnly: false,
+  help: '',
+};
