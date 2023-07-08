74edb9ac53522a5133403b954dc8c78974352473
Kevin <koolkev246@gmail.com>
2023-07-08 17:26:42 -0400

    slash command setnick implemented

--- src/slash-commands/unsorted/setnick.js
+++ src/slash-commands/unsorted/setnick.js
@@ -1,19 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('setnick')
-        .setDescription('Set a user\'s nickname'),
-        async execute(interaction) {
-            await interaction.reply('Pong!');
-        },
-    }
-
-module.exports.info = {
-    name: "setnick",
-    alias: [""],
-    permission: "default",
-    category: "general",
-    guildOnly: false,
-	help: ""
-}
+  data: new SlashCommandBuilder()
+    .setName('setnick')
+    .setDescription("Set the bot's nickname")
+    .addStringOption((option) =>
+      option
+        .setName(`nickname`)
+        .setDescription('The nickname of the bot for this server')
+        .setRequired(true)
+    ),
+  async execute({ client, inter }) {
+    const nick = inter.options.getString('nickname');
+    if (!nick) return inter.reply('Please provide a nickname');
+    inter.guild.me.setNickname(nick);
+    inter.reply(`My nickname has been set to **${nick}**`);
+  },
+};
