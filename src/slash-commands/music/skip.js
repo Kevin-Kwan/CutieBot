72e2d9aa89627bc738e9cc3345e2815235768daa
Kevin <koolkev246@gmail.com>
2024-01-09 22:33:11 -0500

    Fix "Skip" Command Error "InteractionAlreadyReplied"

--- src/slash-commands/music/skip.js
+++ src/slash-commands/music/skip.js
@@ -1,15 +1,24 @@
 module.exports = {
-    name: 'skip',
-    description: 'stop the track',
-    voiceChannel: true,
+  name: 'skip',
+  description: 'stop the track',
+  voiceChannel: true,
 
-    async execute({ inter }) {
-        const queue = player.nodes.get(inter.guildId);
-        await inter.deferReply();
-        if (!queue || !queue.isPlaying()) return inter.reply({ content:`No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
+  async execute({ inter }) {
+    const queue = player.nodes.get(inter.guildId);
+    await inter.deferReply();
 
-        const success = queue.node.skip();
+    if (!queue || !queue.isPlaying())
+      return inter.followUp({
+        content: `No music currently playing ${inter.member}... try again ? ❌`,
+        ephemeral: true,
+      });
 
-        return inter.reply({ content: success ? `Current music ${queue.currentTrack.title} skipped ✅` : `Something went wrong ${inter.member}... try again ? ❌`});
-    },
+    const success = queue.node.skip();
+
+    return inter.followUp({
+      content: success
+        ? `Current music ${queue.currentTrack.title} skipped ✅`
+        : `Something went wrong ${inter.member}... try again ? ❌`,
+    });
+  },
 };
