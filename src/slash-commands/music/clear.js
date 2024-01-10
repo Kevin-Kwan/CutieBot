4e8309c7d62412da8a0558a0493a552a2d01dc6d
Kevin <koolkev246@gmail.com>
2024-01-09 22:31:57 -0500

    Fix "Clear" Command Error "InteractionAlreadyReplied"

--- src/slash-commands/music/clear.js
+++ src/slash-commands/music/clear.js
@@ -1,18 +1,26 @@
 module.exports = {
-    name: 'clear',
-    description: 'clear all the music in the queue',
-    voiceChannel: true,
+  name: 'clear',
+  description: 'clear all the music in the queue',
+  voiceChannel: true,
 
-    async execute({ inter }) {
-        const queue = player.nodes.get(inter.guildId);
-        await inter.deferReply();
+  async execute({ inter }) {
+    const queue = player.nodes.get(inter.guildId);
+    await inter.deferReply();
 
-        if (!queue || !queue.isPlaying()) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
+    if (!queue || !queue.isPlaying())
+      return inter.followUp({
+        content: `No music currently playing ${inter.member}... try again ? ❌`,
+        ephemeral: true,
+      });
 
-        if (!queue.tracks.toArray()[0]) return inter.reply({ content: `No music in the queue after the current one ${inter.member}... try again ? ❌`, ephemeral: true });
+    if (!queue.tracks.toArray()[0])
+      return inter.followUp({
+        content: `No music in the queue after the current one ${inter.member}... try again ? ❌`,
+        ephemeral: true,
+      });
 
-        await queue.tracks.clear();
+    await queue.tracks.clear();
 
-        inter.reply("The queue has just been cleared 🗑️");
-    },
+    inter.followUp('The queue has just been cleared 🗑️');
+  },
 };
