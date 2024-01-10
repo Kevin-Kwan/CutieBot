400a64054224c4ee09f9e3dc699d9fa69446ffd0
Kevin <koolkev246@gmail.com>
2024-01-09 22:43:00 -0500

    Replace inter.reply with inter.followUp

--- src/slash-commands/music/shuffle.js
+++ src/slash-commands/music/shuffle.js
@@ -1,17 +1,27 @@
 module.exports = {
-    name: 'shuffle',
-    description: 'shuffle the track',
-    voiceChannel: true,
+  name: 'shuffle',
+  description: 'shuffle the track',
+  voiceChannel: true,
 
-    async execute({ inter }) {
-        const queue = player.nodes.get(inter.guildId);
-        await inter.deferReply();
-        if (!queue || !queue.isPlaying()) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
+  async execute({ inter }) {
+    const queue = player.nodes.get(inter.guildId);
+    await inter.deferReply();
+    if (!queue || !queue.isPlaying())
+      return inter.followUp({
+        content: `No music currently playing ${inter.member}... try again ? ❌`,
+        ephemeral: true,
+      });
 
-        if (!queue.tracks.toArray()[0]) return inter.reply({ content: `No music in the queue after the current one ${inter.member}... try again ? ❌`, ephemeral: true });
+    if (!queue.tracks.toArray()[0])
+      return inter.followUp({
+        content: `No music in the queue after the current one ${inter.member}... try again ? ❌`,
+        ephemeral: true,
+      });
 
-        await queue.tracks.shuffle();
+    await queue.tracks.shuffle();
 
-        return inter.reply({ content:`Queue shuffled **${queue.tracks.size}** song(s) ! ✅`});
-    },
+    return inter.followUp({
+      content: `Queue shuffled **${queue.tracks.size}** song(s) ! ✅`,
+    });
+  },
 };
--- src/slash-commands/music/volume.js
+++ src/slash-commands/music/volume.js
@@ -20,21 +20,21 @@ module.exports = {
     const queue = player.nodes.get(inter.guildId);
 
     if (!queue)
-      return inter.reply({
+      return inter.followUp({
         content: `No music currently playing ${inter.member}... try again ? ❌`,
         ephemeral: true,
       });
     const vol = inter.options.getNumber('volume');
 
     if (queue.node.volume === vol)
-      return inter.reply({
+      return inter.followUp({
         content: `The volume you want to change is already the current one ${inter.member}... try again ? ❌`,
         ephemeral: true,
       });
 
     const success = queue.node.setVolume(vol);
 
-    return inter.reply({
+    return inter.followUp({
       content: success
         ? `The volume has been modified to **${vol}**/**${maxVol}**% 🔊`
         : `Something went wrong ${inter.member}... try again ? ❌`,
