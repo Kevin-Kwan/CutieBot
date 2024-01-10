2c6f9696178bb871804ae291741aa071a2ef05cf
Kevin <koolkev246@gmail.com>
2024-01-09 22:29:59 -0500

    Fix "InteractionAlreadyReplied" for "stop" command

--- src/slash-commands/music/stop.js
+++ src/slash-commands/music/stop.js
@@ -1,15 +1,22 @@
 module.exports = {
-    name: 'stop',
-    description: 'stop the track',
-    voiceChannel: true,
+  name: 'stop',
+  description: 'stop the track',
+  voiceChannel: true,
 
-    async execute({ inter }) {
-        const queue = player.nodes.get(inter.guildId);
-        await inter.deferReply();
-        if (!queue || !queue.isPlaying()) return inter.reply({ content:`No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
+  async execute({ inter }) {
+    const queue = player.nodes.get(inter.guildId);
+    await inter.deferReply();
+    if (!queue || !queue.isPlaying()) {
+      return inter.followUp({
+        content: `No music currently playing ${inter.member}... try again ? ❌`,
+        ephemeral: true,
+      });
+    }
 
-        queue.delete();
+    queue.delete();
 
-        inter.reply({ content: "Music stopped into this server, see you next time ✅"});
-    },
+    inter.followUp({
+      content: 'Music stopped into this server, see you next time ✅',
+    });
+  },
 };
