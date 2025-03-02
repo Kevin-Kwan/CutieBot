51d55b90c202a31853e756b2e2325daead8ac087
Kevin Kwan <koolkev246@gmail.com>
2025-03-01 22:18:44 -0500

    fix: ensure proper handling of message replies in the say command

--- src/slash-commands/unsorted/say.js
+++ src/slash-commands/unsorted/say.js
@@ -12,8 +12,8 @@ module.exports = {
     async execute({ client, inter }) {
         const saidMessage = inter.options.getString('message');
         // the next two lines make it seem like the bot just talked with no trace of you ever using the command
-        inter.deferReply();
-        inter.deleteReply();
+        await inter.deferReply();
+        await inter.deleteReply();
         inter.channel.send(saidMessage);
     },
 };
